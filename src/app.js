399e4fe0b7020861b27c082c769522558305ccbe
Sonu Kumar <ksonuraj1@gmail.com>
2024-06-09 00:26:22 +0530

    User model creation

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+export const app = express();
+app.use(
+  cors({
+    origin: process.env.ALLLOWED_OROGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: '16kb' }));
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+app.use(express.static('public'));
+app.use(cookieParser());
